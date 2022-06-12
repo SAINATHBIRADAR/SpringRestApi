64dcad7b5fc59888e96d5e02a3f207b2cefa7e63
SAINATHBIRADAR <51283359+SAINATHBIRADAR@users.noreply.github.com>
2022-06-12 11:09:55 +0530

    Update HospitalService.java

--- src/main/java/com/example/Spring/services/HospitalService.java
+++ src/main/java/com/example/Spring/services/HospitalService.java
@@ -4,7 +4,7 @@ import java.util.*;
 import com.example.Spring.entities.Fields;
 
 public interface HospitalService {
-	public int createkey();
+	public Long createkey();
 
 	public List<?> createHash(Fields fields);
 
